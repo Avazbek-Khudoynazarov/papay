dc19f26998a8cd556a0bdb694c101800f33db872
Avazbek-khudoynazarov <avazbek.khudoynazarov@bk.ru>
2023-01-20 02:24:14 +0900

    loyihani express qismini shakillantirib olish

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const mongodb = require("mongodb");
+
+let db;
+const connectionString =
+  "mongodb+srv://Avazbek:0fmOmISx8zsyoDuw@cluster0.x88fytm.mongodb.net/papays";
+mongodb.connect(
+  connectionString,
+  {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection mongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3003;
+      server.listen(PORT, function () {
+        console.log(
+          `server is succesfully running: ${PORT},http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);
