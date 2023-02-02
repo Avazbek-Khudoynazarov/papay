07bd25d117b240ba7980dbd5dc8f061ba3535c9f
Avazbek-khudoynazarov <avazbek.khudoynazarov@bk.ru>
2023-02-02 12:36:52 +0900

    Controller va Multer and L-task

--- train.js
+++ train.js
@@ -1,5 +1,7 @@
-function FootBallPoints(wins, draws, losses) {
-  return wins * 3 + draws * 1 + losses * 0;
-}
-const result = FootBallPoints(3, 4, 2);
-console.log(result);
+const calculate = "4*2";
+console.log(eval(calculate));
+// function FootBallPoints(wins, draws, losses) {
+//   return wins * 3 + draws * 1 + losses * 0;
+// }
+// const result = FootBallPoints(3, 4, 2);
+// console.log(result);
