61afa745f1673f8afbca8dc525067aa6e184f5a4
Avazbek-khudoynazarov <avazbek.khudoynazarov@bk.ru>
2023-02-04 14:12:34 +0900

    feat: Schema Model va Service Model and M-task

--- train.js
+++ train.js
@@ -1,5 +1,30 @@
-const calculate = "4*2";
-console.log(eval(calculate));
+class Member {
+  #counts;
+
+  constructor(sanoq) {
+    this.#counts = sanoq;
+  }
+
+  addMember(member) {
+    this.#counts = this.#counts + member;
+  }
+
+  removeMember(member) {
+    this.#counts = this.#counts - member;
+  }
+
+  inform() {
+    console.log(`memberlar soni ${this.#counts}`);
+  }
+}
+
+const member = new Member(5);
+member.removeMember(2);
+
+member.inform();
+
+// const calculate = "11/10";
+// console.log(eval(calculate));
 // function FootBallPoints(wins, draws, losses) {
 //   return wins * 3 + draws * 1 + losses * 0;
 // }
