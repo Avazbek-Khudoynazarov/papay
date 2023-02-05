8a25d7e3a015e33f23bfc5f28efcf29e29d3ff44
Avazbek-khudoynazarov <avazbek.khudoynazarov@bk.ru>
2023-02-05 15:08:11 +0900

    feat: Service Model and EJS main page

--- models/Product.js
+++ models/Product.js
@@ -5,14 +5,28 @@ const ProductModel = require("../schema/product.model");
 
 class Product {
   constructor() {
-    this.ProductModel = ProductModel;
+    this.productModel = ProductModel;
+  }
+
+  async getAllProductsDataResto(member) {
+    try {
+      member._id = shapeIntoMongooseObjectId(member._id);
+      const result = await this.productModel.find({
+        restaurant_mb_id: member._id,
+      });
+      assert.ok(result, Definer.general_err1);
+      console.log("result", result);
+      return result;
+    } catch (err) {
+      throw err;
+    }
   }
 
   async addNewProductData(data, member) {
     try {
       data.restaurant_mb_id = shapeIntoMongooseObjectId(member._id);
 
-      const new_product = new this.ProductModel(data);
+      const new_product = new this.productModel(data);
       const result = await new_product.save();
 
       assert.ok(result, Definer.product_err1);
@@ -27,15 +41,13 @@ class Product {
       id = shapeIntoMongooseObjectId(id);
       mb_id = shapeIntoMongooseObjectId(mb_id);
 
-      const result = await this.ProductModel.findOneAndUpdate(
-        { _id: id, restaurant_mb_id: mb_id },
-        updated_data,
-        {
+      const result = await this.productModel
+        .findOneAndUpdate({ _id: id, restaurant_mb_id: mb_id }, updated_data, {
           runValidators: true,
           lean: true,
           returnDocument: "after",
-        }
-      ).exec();
+        })
+        .exec();
 
       assert.ok(result, Definer.general_err1);
       return result;
--- router_bssr.js
+++ router_bssr.js
@@ -8,16 +8,18 @@ const uploader_product = require("./utils/upload.multer")("products");
  *        BSSR EJS         *
  ***************************/
 
+router_bssr.get("/", restaurantController.home);
+
 router_bssr
-  .get("/signup", restaurantController.getSignupMyRestaurant)
-  .post("/signup", restaurantController.signupProcess);
+  .get("/sign-up", restaurantController.getSignupMyRestaurant)
+  .post("/sign-up", restaurantController.signupProcess);
 router_bssr
   .get("/login", restaurantController.getLoginMyRestaurant)
   .post("/login", restaurantController.loginProcess);
 router_bssr.get("/logout", restaurantController.logout);
 router_bssr.get("/check-me", restaurantController.checkSessions);
 
-router_bssr.get("/products/menu", restaurantController.getMyRestaurantData);
+router_bssr.get("/products/menu", restaurantController.getMyRestaurantProducts);
 router_bssr.post(
   "/products/create",
   restaurantController.validateAuthRestaurant,
