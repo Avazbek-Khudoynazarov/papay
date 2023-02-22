8b36265323023848538082ee2f36a769d41e54ef
Avazbek-khudoynazarov <avazbek.khudoynazarov@bk.ru>
2023-02-22 20:57:20 +0900

    Task complited

--- train.js
+++ train.js
@@ -1,27 +1,36 @@
-class Member {
-  #counts;
-
-  constructor(sanoq) {
-    this.#counts = sanoq;
-  }
-
-  addMember(member) {
-    this.#counts = this.#counts + member;
-  }
-
-  removeMember(member) {
-    this.#counts = this.#counts - member;
-  }
-
-  inform() {
-    console.log(`memberlar soni ${this.#counts}`);
-  }
+function findDays(first, second) {
+  const num1 = new Date(first);
+  const num2 = new Date(second);
+  const diffTime = Math.abs(num2 - num1);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  return diffDays;
 }
+console.log(findDays("12/13/2022", "12/13/2023"));
 
-const member = new Member(5);
-member.removeMember(2);
+// class Member {
+//   #counts;
 
-member.inform();
+//   constructor(sanoq) {
+//     this.#counts = sanoq;
+//   }
+
+//   addMember(member) {
+//     this.#counts = this.#counts + member;
+//   }
+
+//   removeMember(member) {
+//     this.#counts = this.#counts - member;
+//   }
+
+//   inform() {
+//     console.log(`memberlar soni ${this.#counts}`);
+//   }
+// }
+
+// const member = new Member(5);
+// member.removeMember(2);
+
+// member.inform();
 
 // const calculate = "11/10";
 // console.log(eval(calculate));
