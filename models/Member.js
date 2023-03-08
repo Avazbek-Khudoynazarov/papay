c2be8e88514ff61a2a877b00a9480f9b1eaa4c9d
Avazbek-khudoynazarov <avazbek.khudoynazarov@bk.ru>
2023-03-08 23:28:48 +0900

    feat: getChosenMember and getAllProducts

--- models/Member.js
+++ models/Member.js
@@ -2,6 +2,8 @@ const MemberModel = require("../schema/member.model");
 const Definer = require("../lib/mistake");
 const assert = require("assert");
 const bcrypt = require("bcryptjs");
+const { shapeIntoMongooseObjectId } = require("../lib/config");
+const View = require("../models/View");
 
 class Member {
   constructor() {
@@ -45,6 +47,55 @@ class Member {
       assert.ok(isMatch, Definer.auth_err4);
 
       return await this.memberModel.findOne({ mb_nick: input.mb_nick }).exec();
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getChosenMemberData(member, id) {
+    try {
+      id = shapeIntoMongooseObjectId(id);
+
+      console.log("member:::", member);
+
+      if (member) {
+        // condition if not seen before
+        await this.viewChosenItemByMember(member, id, "member");
+      }
+
+      const result = await this.memberModel
+        .aggregate([
+          { $match: { _id: id, mb_status: "ACTIVE" } },
+          { $unset: "mb_password" },
+        ])
+        .exec();
+
+      assert.ok(result, Definer.general_err2);
+      return result[0];
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async viewChosenItemByMember(member, view_ref_id, group_type) {
+    try {
+      view_ref_id = shapeIntoMongooseObjectId(view_ref_id);
+      const mb_id = shapeIntoMongooseObjectId(member._id);
+
+      const view = new View(mb_id);
+      const isValid = await view.validateChosenTarget(view_ref_id, group_type);
+      assert.ok(isValid, Definer.general_err2);
+
+      //logged user has seen target before
+      const doesExist = await view.checkViewExistence(view_ref_id);
+      console.log("doesExist:::", doesExist);
+
+      if (!doesExist) {
+        const result = await view.insertMemberView(view_ref_id, group_type);
+        assert.ok(result, Definer.general_err1);
+      }
+
+      return true;
     } catch (err) {
       throw err;
     }
--- router.js
+++ router.js
@@ -1,23 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const memberController = require("./controllers/memberController");
+const productController = require("./controllers/productController");
 
 /***************************
  *        REST API         *
  ***************************/
 
-//member dahildor routers
+// Member related routers
 router.post("/signup", memberController.signup);
 router.post("/login", memberController.login);
 router.get("/logout", memberController.logout);
 router.get("/check-me", memberController.checkMyAuthentication);
+router.get(
+  "/member/:id",
+  memberController.retrieveAuthMember,
+  memberController.getChosenMember
+);
 
-//other routers
-router.get("/menu", (req, res) => {
-  res.send("Menu sahifadasiz");
-});
-router.get("/community", (req, res) => {
-  res.send("Jamiyat sahifadasiz");
-});
+// Product related routers
+router.post(
+  "/products",
+  memberController.retrieveAuthMember,
+  productController.getAllProducts
+);
 
 module.exports = router;
